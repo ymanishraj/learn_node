e0839a19b28e7941d69c9da61c8a1d5c99c87891
Manishraj Yadav <=>
2021-10-27 10:59:47 +0545

    First Commit

---
+++ modules.js
@@ -0,0 +1,8 @@
+const names = require('./names')
+const sayHi = require('./utils')
+const data = require('./items')
+require('./mind-granade')
+
+sayHi('Hello world')
+sayHi(names.john)
+sayHi(names.peter)
---
+++ names.js
@@ -0,0 +1,8 @@
+// these are shared 
+const john = 'john'
+const peter = 'peter'
+
+// this is local to this file
+const secret = 'super secrete'
+
+module.exports = { john, peter }
